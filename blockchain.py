328195f1fdd43482c0fb409d3db1154656e43b42
zackprogrammer <simonbabaela@gmail.com>
2021-02-02 13:47:26 -0500

    added comments for small brain people

--- blockchain.py
+++ blockchain.py
@@ -2,28 +2,30 @@
 
 import pprint
 import hashlib
-# Extremely basic setup of the blockchain
-
+#Block class
 class Block:
     def __init__(self, index, timestamp, previhash = None):
         self.previhash = previhash
         self.index = index
         self.timestamp = timestamp
         self.hash = self.calculatehash()
-      
+    # calculate the hash
     def calculatehash(self):
         d = str("{}{}{}".format(self.previhash, self.index, self.timestamp))
+        #returns the sha256 hash
         return hashlib.sha256(d.encode()).hexdigest()
 
-
+#blockchain
 class BlockChain:
     def __init__(self, chain = []):
         self.chain = chain
 
+    #gets the previous hash    
     def getprevihash(self):
         return self.chain[-1]
-
+    #adds block to our blockchain    
     def addblock(self, block):
+        #if the blokc chain does not have a transactino then the previous hahs is none making it the genisis block
         if len(self.chain) > 0:
             block.previhash = self.getprevihash().hash
             self.chain.append(block)
@@ -31,6 +33,7 @@ class BlockChain:
             block.previhash = None 
             self.chain.append(block)
 
+    # Makes us a nicer way of seeing our blockchain
     def printchain(self):
             for block in self.chain:
                 jsonarray = {}
@@ -39,6 +42,6 @@ class BlockChain:
                 jsonarray['prevhash'] = block.previhash
                 jsonarray['index'] = block.index
                 jsonarray['timestamp'] = block.timestamp
-
+                # Prints out the blockchain
                 pprint.pprint(jsonarray)
 
